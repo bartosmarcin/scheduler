8d1cddf478fd5039cd4dc198ab67d9651c469e00
bartosmarcin <bartosmarcin@gmail.com>
2012-12-13 18:07:07 +0100

    Added contructor and enlargeHeap method

--- src/scheduler/PriorityQueue.java
+++ src/scheduler/PriorityQueue.java
@@ -5,6 +5,22 @@ package scheduler;
  * 
  * @author Marcin Bartoś
  */ 
-public class PriorityQueue { 
+public class PriorityQueue <T extends Comparable>{ 
+    
+    private Object[] heap;
+    int size;
+    
+    public PriorityQueue(int initLength){
+        size = 0;
+        if( initLength < 1)
+            initLength = 1;
+        heap = new Object[initLength];
+    }
+    
+    private void enlargeHeap(){
+        Object[] temp = heap;
+        heap = new Object[temp.length << 1];
+        System.arraycopy(temp, 0, heap, 0, temp.length);
+    }
 
 } 
