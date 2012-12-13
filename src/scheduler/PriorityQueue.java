c6fbd1acd48827306efca0f4a33128309ddf45d5
bartosmarcin <bartosmarcin@gmail.com>
2012-12-13 18:54:32 +0100

    Added add, pushUp, getParentIndex methods,

--- src/scheduler/PriorityQueue.java
+++ src/scheduler/PriorityQueue.java
@@ -8,13 +8,42 @@ package scheduler;
 public class PriorityQueue <T extends Comparable>{ 
     
     private Object[] heap;
-    int size;
+    private int size;
     
     public PriorityQueue(int initLength){
         size = 0;
         if( initLength < 1)
             initLength = 1;
         heap = new Object[initLength];
+    }
+    
+    public void add(T newNode){
+        if(newNode == null)
+            throw new IllegalArgumentException("Cannot add null");
+        if(size >= heap.length)
+            enlargeHeap();
+        if(size == 0)
+            heap[0] = newNode;
+        else
+            pushUp(size, newNode);
+        size++;
+    }
+    
+    public void pushUp(int index, T node){
+        int parentIndex;
+        while( index > 0)
+        {
+            parentIndex = getParentIndex(index);
+            if( node.compareTo( heap[parentIndex] ) >= 0 )
+                break;
+            heap[index] = heap[parentIndex];
+            index = parentIndex;
+        }
+        heap[index] = node;
+    }
+    
+    private int getParentIndex(int index){ //TODO reconsider need of this
+        return (index-1) >> 1;
     }
     
     private void enlargeHeap(){
